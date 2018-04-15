92235433e0a56435357b563ad43faa138afa07d2
Alan Nguyen <alan@mirk.co>
2018-04-15 22:54:06 +1000

    First commit

---
+++ cards/deck.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// Create a new type of deck
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+func (d deck) print() {
+	for _, card := range d {
+		fmt.Println(card)
+	}
+}
